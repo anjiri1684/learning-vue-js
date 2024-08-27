e1fed79440fd0b1c596386c390f78a4391344952
vincent anjiri <vincentanjiri12@gmail.com>
2024-08-27 10:40:38 +0300

    "Refactor app.js, index.html, and styles.css to replace course goal functionality with event handling and counter increment/decrement"

--- app.js
+++ app.js
@@ -1,21 +1,31 @@
+// const app = Vue.createApp({
+//   data() {
+//     return {
+//       courseGoal1: "Lean vue and start practicing the acquired skills",
+//       courseGoal2: "Master Vue and build amazing apps",
+//       vueLink: "www.vincentanjiri.co.ke",
+//     };
+//   },
+//   methods: {
+//     outputGoal: function () {
+//       const randomNumber = Math.random();
+//       if (randomNumber > 0.5) {
+//         return this.courseGoal1;
+//       } else {
+//         return this.courseGoal2;
+//       }
+//     },
+//   },
+// });
+
+// app.mount("#user-goal");
+
 const app = Vue.createApp({
   data() {
     return {
-      courseGoal1: "Lean vue and start practicing the acquired skills",
-      courseGoal2: "Master Vue and build amazing apps",
-      vueLink: "www.vincentanjiri.co.ke",
+      counter: 0,
     };
-  },
-  methods: {
-    outputGoal: function () {
-      const randomNumber = Math.random();
-      if (randomNumber > 0.5) {
-        return this.courseGoal1;
-      } else {
-        return this.courseGoal2;
-      }
-    },
   },
 });
 
-app.mount("#user-goal");
+app.mount("#events");
